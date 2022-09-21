5983a3918f3fd123395b75b58711eb541a99cc12
Chris Paliqaw <christophomos@gmail.com>
2022-09-21 08:42:38 +0000

    feature: ROS2 linear algebra package compiles successfully

---
+++ include/linear_algebra_tools/linear_algebra.hpp
@@ -0,0 +1,14 @@
+#ifndef LINEAR_ALGEBRA_UTILITIES_LINEAR_ALGEBRA_HPP_
+#define LINEAR_ALGEBRA_UTILITIES_LINEAR_ALGEBRA_HPP_
+
+#include <rclcpp/rclcpp.hpp>
+#include "geometry_msgs/msg/twist.hpp"
+#include <tf2_geometry_msgs/tf2_geometry_msgs.h>
+
+namespace linear_algebra_utilities {
+
+geometry_msgs::msg::Vector3 euler_from_quaternion(tf2::Quaternion q);
+double magnitude(geometry_msgs::msg::Vector3 v3);
+
+} // namespace linear_algebra_utilities
+#endif // LINEAR_ALGEBRA_UTILITIES_LINEAR_ALGEBRA_HPP_
---
+++ src/main.cpp
@@ -0,0 +1,6 @@
+#include "linear_algebra.hpp"
+
+int main() {
+
+  return 0;
+}
